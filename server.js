f4ed580c54dc26a9a7f9af9c22fe92ffebe1f415
nassimblddd <nassim.belaid@aiesec.com>
2025-02-07 16:15:25 +0100

    comments

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// Importation de la bibliothèque Express
 import express from "express";
 
+// Création d'une instance de l'application Express
 const app = express();
-app.use(express.json())
 
-app.get("/",(req,res) => {
-    console.log(req)
+// Ajout d'un middleware pour analyser les requêtes avec des charges utiles JSON
+app.use(express.json());
+
+// Définition d'un gestionnaire de route pour les requêtes GET à l'URL racine ("/")
+app.get("/", (req, res) => {
+    // Affichage de l'objet requête dans la console
+    console.log(req);
 });
 
-app.listen("5000", () => {console.log("listening on the port 5000")})
+// Démarrage du serveur et écoute sur le port 5000
+app.listen("5000", () => {
+    // Affichage d'un message dans la console lorsque le serveur démarre
+    console.log("listening on the port 5000");
+});
