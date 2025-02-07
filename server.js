276ee097a6444703b86226080bb1096be93589cd
nassimblddd <nassim.belaid@aiesec.com>
2025-02-07 16:11:05 +0100

    install dependencies

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 app.use(express.json())
 
-app.get("/",(req,res) => {console.log(req)});
+app.get("/",(req,res) => {
+    console.log(req)
+});
 
 app.listen("5000", () => {console.log("listening on the port 5000")})
