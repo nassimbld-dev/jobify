e34180654050330fa9d2d18b81f0fa97e0f5b097
nassimblddd <nassim.belaid@aiesec.com>
2025-02-14 20:07:25 +0100

    start with endpoints

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 // Importation de la bibliothèque Express
 import express from "express";
-
 // Création d'une instance de l'application Express
 const app = express();
+import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let jobs = [
+    {id: nanoid(), title: "Software Developer", company: "Google"},
+    {id: nanoid(), title: "Frontend Developer", company: "Facebook"},]
+
+if(process.env.NODE_ENV === "development") {
+    // Ajout d'un middleware pour afficher les journaux de requête en mode développement
+    app.use(morgan("dev"));
+}
 
 // Ajout d'un middleware pour analyser les requêtes avec des charges utiles JSON
 app.use(express.json());
 
-// Définition d'un gestionnaire de route pour les requêtes GET à l'URL racine ("/")
-app.get("/", (req, res) => {
-    // Affichage de l'objet requête dans la console
-    console.log(req);
+// get all jobs
+app.get("/api/v1/jobs", (req, res) => {
+    res.status(200).json({
+            jobs,
+    });
 });
 
-// Démarrage du serveur et écoute sur le port 5000
-app.listen("5000", () => {
-    // Affichage d'un message dans la console lorsque le serveur démarre
-    console.log("listening on the port 5000");
+// create new job, a tester sur postman : body, row
+app.post("/api/v1/jobs", (req, res) => {
+    const { title, company} = req.body;
+    console.log(title, company);
+    if(!title || !company) {
+        return res.status(400).json({
+            error: "title and company are required",
+        });
+        return;
+    }
+
+    const id = nanoid(10); 
+    const newJob = {id, title, company};
+
+    jobs.push(newJob);
+
+    res.status(201).json(newJob);
+});
+
+
+
+const PORT = process.env.PORT || 5100;
+app.get("/", (req, res) => {
+    res.send('Hi');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${5100}`);
 });
