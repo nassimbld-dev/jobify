cbe95c97d760f1c21aaf529c10c5b254e6013f02
nassimblddd <nassim.belaid@aiesec.com>
2025-02-15 12:05:37 +0100

    crud and routes

--- server.js
+++ server.js
@@ -45,12 +45,63 @@ app.post("/api/v1/jobs", (req, res) => {
     res.status(201).json(newJob);
 });
 
+//get one job
+app.get('/api/v1/jobs/:id',(req,res) => {
+    const {id}= req.params;
+    console.log(id);
+    //avec les accolades ça ne fonctionne pas parce que return avec accolades(dans notre cas pas de return)
+    //deuxième problème type 
+const job= jobs.find((i)=>i.id==id)
+console.log(jobs[0].id )
+if(!job){
+    return res.status(404).json({message: 'job not found'});
+}
+res.status(201).json(job);
+  
+ })
 
+
+app.delete('api/v1/jobs', (req,res) =>{
+    const {id} = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ error: "Job not found" });
+    }
+    jobs = jobs.filter((job) => job.id !== id);
+    res.status(200).json({message: "Job deleted successfully"});
+})
+
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, company } = req.body;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ error: "Job not found" });
+    }
+    job.title = title || job.title;
+    job.company = company || job.company;
+    res.status(200).json(job);
+});
+
+// gestion des routes inexistantes
+app.use('*', (req, res) => {
+    res.status(404).json({
+        error: "Route not found",
+    });
+});
+
+// gestion des erreurs serveur
+app.use((err, req, res, next) => {
+    console.error(err); 
+    res.status(500).json({
+        error: "Something went wrongr",
+    });
+});
 
 const PORT = process.env.PORT || 5100;
 app.get("/", (req, res) => {
     res.send('Hi');
 });
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${5100}`);
+    console.log(`Server is running on port ${PORT}`);
 });
